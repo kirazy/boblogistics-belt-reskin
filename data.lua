e14dc726d240804418d39e70584737dbe72d299c
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-02-09 17:01:35 -0800

    Copyright additions

--- data.lua
+++ data.lua
@@ -1,3 +1,8 @@
+-- Copyright (c) 2020 Kirazy
+-- Part of Bob's Logistics Belt Reskin
+--     
+-- See LICENSE.md in the project directory for license information.
+
 require("prototypes.functions")
 require("prototypes.entity.belt-particles")
 require("prototypes.entity.belt-explosions")
